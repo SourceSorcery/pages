24fc64a4d9d47cb9b9ffa9aadb987e6ed8c36272
SourceSorcery <mfergason@jacobscorp.com>
2022-06-27 16:52:46 -0500

    added animation

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ $(document).ready(readyFunction);
 function readyFunction( jQuery ){
   console.log('document ready');
   $('#p1').html('I was modified by jquery!');
+  animateH1();
 }
 
+function animateH1(){
+ $('#h1ani').fadeTo(1000,1);
+}
