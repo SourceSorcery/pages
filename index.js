4b72b1d21ae94d136ce41073e88a9c720756e1db
SourceSorcery <mfergason@jacobscorp.com>
2022-06-27 16:18:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ console.log('testing js');
 $( document ).ready(readyFunction);
 
 function readyFunction( jQuery ){
+  $console.log('document ready');
   $('#p1').html('I was modified by jquery!');
 }
 
