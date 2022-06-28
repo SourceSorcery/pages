4a1984fb507838e54c9ccb5e9da62c5e025ffa07
SourceSorcery <mfergason@jacobscorp.com>
2022-06-28 09:18:13 -0500

    Fade In Title - 1
    trying to fade in the title by letter
    attempt 1

--- index.js
+++ index.js
@@ -5,9 +5,26 @@ $(document).ready(readyFunction);
 function readyFunction( jQuery ){
   console.log('document ready');
   $('#p1').html('I was modified by jquery!');
-  animateH1();
+  //animateH1();
+  fadeInWord("Hello World!","#h1title");
 }
 
 function animateH1(){
  $('#h1ani').fadeTo(1000,1);
+}
+
+
+function fadeInWord(word, wrapperElementSelector){
+  var len = word.length;
+  for (i=0;i<len;i++){
+    var id = "word" + i;
+    app = "<span id='" + id + "' style='opacity:0;'>" + word[i] + "</span>";
+    setTimeout(function(){appendLetter(app,id)},i*100);
+  }
+}
+
+function appendLetter(html, idToAnimate){
+  $(wrapperElementSelector).append(html);
+  var thisid = "#" + idToAnimate;
+  $(thisid).fadtTo(1000,1);
 }
