57cbf547580b8bcdd4ebc77bdf3b2cc49fc311be
SourceSorcery <mfergason@jacobscorp.com>
2022-06-29 16:04:11 -0500

    More work on new w3 css home page

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ $(document).ready(readyFunction);
 function readyFunction( jQuery ){
   console.log('document ready');
   $('#p1').html('I was modified by jquery!');
-  fadeInWord("Hello World!","#h1title");
+  fadeInWord("Mark Fergason's GitHub Pages","#h1title");
 }
 
 function fadeInWord(word, wrapperElementSelector){
   for (let i=0;i<word.length;i++){
     let letterid = "word" + i;
     let app = "<span id='" + letterid + "' style='opacity:0;'>" + word[i] + "</span>";
-    setTimeout(function(){appendLetter(app,letterid,wrapperElementSelector)},i*100);
+    setTimeout(function(){appendLetter(app,letterid,wrapperElementSelector)},i*50);
   }
 }
 
 function appendLetter(html, idToAnimate, wrapper){
   $(wrapper).append(html);
   var thisid = "#" + idToAnimate;
-  $(thisid).fadeTo(1000,1);
+  $(thisid).fadeTo(500,1);
 }
